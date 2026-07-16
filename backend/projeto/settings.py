008122296b39e267e7e62d08cc629d49f97725b3
lucassbertol <lb024347@gmail.com>
2026-07-16 13:20:20 -0300

    add env.exemplo

--- backend/projeto/settings.py
+++ backend/projeto/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = django-insecure-rb3uhkzmb1j%d3kvb&o5)42)#++2%&^wk51sgagep6aka(4m3j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
